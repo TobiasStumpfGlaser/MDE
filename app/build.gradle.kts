49da6e644f26eb3723c66949f3ced1bc20ecdb68
Tobias Stumpf <t.stumpf@brainware-solutions.de>
2026-05-27 09:47:23 +0200

    V5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.mde"
         minSdk = 24
         targetSdk = 36
-        versionCode = 74
-        versionName = "5.0"
+        versionCode = 75
+        versionName = "5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
